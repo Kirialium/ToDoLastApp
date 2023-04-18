e3606586f82c11d00982923e7d02c6624e8c9247
Kirill <csgokirilll@mail.ru>
2023-04-18 16:02:46 +0500

    second activity first variation

--- app/src/main/java/com/example/todoapplic/MainActivity.java
+++ app/src/main/java/com/example/todoapplic/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.todoapplic;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton newNoteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +21,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-
-
         //Установка цвета для верхней и нижней полоски
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(this.getResources().getColor(R.color.third_blue));
-        getWindow().setNavigationBarColor(getResources().getColor(R.color.first_blue));
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.white));
 
-
+        //Кнопка перехода на создание новой заметки
+        newNoteBtn = findViewById(R.id.new_note_btn);
+        newNoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewNoteActivity.class);
+                MainActivity.this.startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/todoapplic/NewNoteActivity.java
+++ app/src/main/java/com/example/todoapplic/NewNoteActivity.java
@@ -1,14 +1,49 @@
 package com.example.todoapplic;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 public class NewNoteActivity extends AppCompatActivity {
+
+    ImageButton backToHomeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        //Установка цвета для верхней и нижней полоски
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.third_blue));
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.white));
+
+        //Кнопка возвращения домой
+        backToHomeBtn = findViewById(R.id.back_to_home_btn);
+        backToHomeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NewNoteActivity.this, MainActivity.class);
+                NewNoteActivity.this.startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+        });
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
